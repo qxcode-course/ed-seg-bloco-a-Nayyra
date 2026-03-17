56d4cb4cef7247b47ab37ad9812d5f30d6113ad8
Kethillyn <naykethillyn@gmail.com>
2026-03-17 16:23:02 -0300

    fazendo as questões com o wsl(terminei pedra na lua)

--- tasks/ed/pedra/go/draft.go
+++ tasks/ed/pedra/go/draft.go
@@ -4,17 +4,35 @@ import "math"
 func main() {
     var n int
     fmt.Scan(&n)
-    jogadores := make(map([int]int))
+    // pontos := make([]float64, n)
+    mapeamento := make(map[int]float64)
 
-    for i := range n{
+    for i := 0; i <= n; i++{
         var pa, pb float64
         fmt.Scan(&pa, &pb)
-
-        diferenca := math.Abs(pa-pb)
-        if (diferenca > 10){
-            jogadores[i] = diferenca
+        if (pa >= 10) && (pb  >= 10){
+        
+            diferenca := math.Abs(pa-pb)
+            mapeamento[i] = diferenca
+            
         }
     }
 
-    fmt.Print(jogadores)
+    menor := 10.0
+    vencendor := 0
+
+
+    for i, a := range mapeamento{
+        if (a <= menor){
+            vencendor = i
+        }
+    }
+    
+    if (len(mapeamento) == 0){
+        fmt.Println("sem ganhador")
+    }else {
+        
+        fmt.Println(vencendor)
+    }
+
 }
