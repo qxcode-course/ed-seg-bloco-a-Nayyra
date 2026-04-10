3116d49216b51d32025d92f090956714e3ab6211
Kethillyn <naykethillyn@gmail.com>
2026-04-10 08:57:16 -0300

    segundo desenho

--- tasks/ed/fractais_png/src/go/main.go
+++ tasks/ed/fractais_png/src/go/main.go
@@ -9,43 +9,25 @@ func randInt(min, max int) int {
 	return min + rand.Intn(max-min+1)
 }
 
-func arvore(pen *Pen, dis float64){
-	angle := 30.0
+func estrela(pen *Pen, dis float64){
 
-	if dis < 10{
-		pen.SetRGB(0, 0, 0)
+	if dis < 2{
 		return
 	}
 
-	pen.Walk(dis)
-	pen.Left(angle)
-	arvore(pen, dis - 5)
-	pen.Right(2*angle)
-	arvore(pen, dis - 5)
-	pen.Left(angle)
-	pen.Walk(-dis)
-
-	
+	for range 5{
+		pen.Walk(dis)
+		estrela(pen, dis * 0.4)
+		pen.Walk(-dis)
+		pen.Left(72)
+	}
 }
 
 func main() {
-	pen := NewPen(800, 600)
-	pen.SetPosition(540, 300)
-	pen.SetHeading(9)
-	arvore(pen, 45.0)
-	pen.Left(450)
-	arvore(pen, 45.0)
-	pen.Left(490)
-	pen.Walk(170)
-	pen.Right(-630)
-	pen.Walk(170)
-	pen.SetHeading(90)
-	arvore(pen, 45.0)
-	pen.Left(450)
-	arvore(pen, 45.0)
-	pen.Left(630)
-	
-	
+	pen := NewPen(800, 800)
+	pen.SetPosition(400, 400)
+	pen.SetHeading(50)
+	estrela(pen, 150)
 	pen.SavePNG("tree.png")
 	fmt.Println("PNG file created successfully.")
 }
