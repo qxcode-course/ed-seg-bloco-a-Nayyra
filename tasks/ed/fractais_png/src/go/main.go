2cab6e4516cad4ac68b0ef8f2bdcc04b0f7cd70b
Kethillyn <naykethillyn@gmail.com>
2026-04-10 10:37:14 -0300

    rotação

--- tasks/ed/fractais_png/src/go/main.go
+++ tasks/ed/fractais_png/src/go/main.go
@@ -2,32 +2,35 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"math/rand/v2"
 )
 
-func randInt(min, max int) int {
-	return min + rand.Intn(max-min+1)
-}
+// func randInt(min, max int) int {
+// 	return min + rand.Intn(max-min+1)
+// // }
 
 func estrela(pen *Pen, dis float64){
 
-	if dis < 2{
+	r := rand.Float64() * 250
+	g := rand.Float64() * 250
+	b := rand.Float64() * 250
+
+	if dis < 5{
 		return
 	}
+	pen.Left(90)
+	pen.SetRGB(r, g, b)
+	pen.SetLineWidth(4)
+	pen.Walk(dis)
+	estrela(pen, dis - 10)
 
-	for range 5{
-		pen.Walk(dis)
-		estrela(pen, dis * 0.4)
-		pen.Walk(-dis)
-		pen.Left(72)
-	}
 }
 
 func main() {
 	pen := NewPen(800, 800)
-	pen.SetPosition(400, 400)
-	pen.SetHeading(50)
-	estrela(pen, 150)
+	pen.SetPosition(700, 700)
+	pen.SetHeading(0)
+	estrela(pen, 600)
 	pen.SavePNG("tree.png")
 	fmt.Println("PNG file created successfully.")
 }
